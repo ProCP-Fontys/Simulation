f6e42b619220f8c63230f2c9e2ad502e26bcb39e
Mikaeil1990 <m.shaghelanilor@gmail.com>
2016-01-26 13:45:57 +0100

    FINAEL VERSION
    presenting this

--- Project/EntryInformation/Help.Designer.cs
+++ Project/EntryInformation/Help.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = global::EntryInformation.Properties.Resources.Untitled;
+            this.BackgroundImage = global::EntryInformation.Properties.Resources.Untitled1;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1202, 721);
             this.Name = "Help";
--- Project/EntryInformation/Properties/Resources.Designer.cs
+++ Project/EntryInformation/Properties/Resources.Designer.cs
@@ -249,5 +249,15 @@ namespace EntryInformation.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Untitled1 {
+            get {
+                object obj = ResourceManager.GetObject("Untitled1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
